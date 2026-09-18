e9f1d6e779f42b7d81f49a4a9d92048bb8cd36eb
Yannick Weiss <yannick.weiss@bob.ch>
2026-09-18 16:15:04 +0200

    Initial Commit

---
+++ willitrain/willitrainApp.swift
@@ -0,0 +1,17 @@
+//
+//  willitrainApp.swift
+//  willitrain
+//
+//  Created by yannick on 18.09.2026.
+//
+
+import SwiftUI
+
+@main
+struct willitrainApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
